a1395c697d334129384f84256db8f454ba2b3f81
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2024-10-05 21:52:07 +0300

    Add play timer

--- Assets/Scripts/UI/GameplayUi.cs
+++ Assets/Scripts/UI/GameplayUi.cs
@@ -1,3 +1,4 @@
+using Scripts.Characters;
 using Scripts.Managers;
 using Scripts.Utils;
 using System.Collections;
@@ -11,6 +12,7 @@ namespace Scripts.UI
     public class GameplayUi : MonoBehaviour
     {
         [SerializeField] private Slider _progressBar;
+        [SerializeField] private TMP_Text _countdown;
         [SerializeField] private TMP_Text _timer;
         [SerializeField] private TMP_Text _counter;
         [SerializeField] private GameObject _tutorialHint;
@@ -20,7 +22,9 @@ namespace Scripts.UI
         [SerializeField] private Color _timerEndColor;
 
         private MatchManager _matchManager;
+        private Coroutine _countdownRoutine;
         private Coroutine _timerRoutine;
+        private WaitForEndOfFrame _waitForEndOfFrame = new();
 
         [Inject]
         private void Construct(MatchManager matchManager)
@@ -39,29 +43,39 @@ namespace Scripts.UI
         {
             GameManager.OnAfterStateChanged += OnAfterStateChanged;
             MatchManager.OnMatchStarted += OnMatchStarted;
+            MatchManager.OnMatchEnded += StopTimer;
             MatchManager.OnCoinsAmountChanged += OnCoinsAmountChanged;
+            Player.OnPlayerLost += StopTimer;
         }
 
         private void Unsubscribe()
         {
             GameManager.OnAfterStateChanged -= OnAfterStateChanged;
             MatchManager.OnMatchStarted -= OnMatchStarted;
+            MatchManager.OnMatchEnded -= StopTimer;
             MatchManager.OnCoinsAmountChanged -= OnCoinsAmountChanged;
+            Player.OnPlayerLost -= StopTimer;
         }
 
         private void OnAfterStateChanged(GameState state)
         {
             if (state != GameState.Gameplay) return;
 
-            if (_timerRoutine == null)
-                _timerRoutine = StartCoroutine(Timer());
+            if (_countdownRoutine == null)
+                _countdownRoutine = StartCoroutine(Countdown());
         }
 
         private void OnMatchStarted()
         {
-            _timer.gameObject.SetActive(false);
+            _countdown.gameObject.SetActive(false);
             _tutorialHint.SetActive(false);
+            _timer.gameObject.SetActive(true);
+
+            if (_timerRoutine == null)
+                _timerRoutine = StartCoroutine(Timer());
         }
+
+        private void StopTimer() => this.StopCoroutine(ref _timerRoutine);
 
         private void OnCoinsAmountChanged(int coins)
         {
@@ -69,38 +83,59 @@ namespace Scripts.UI
             RefreshCoinsCounter();
         }
 
-        private IEnumerator Timer()
+        private IEnumerator Countdown()
         {
-            var timer = _matchManager.TimerOnstart;
+            var countdown = _matchManager.TimerOnstart;
 
-            while (timer > 0f)
+            while (countdown > 0f)
             {
-                var roundedTimer = Mathf.RoundToInt(timer);
-                var actualText = roundedTimer >= 1f ? $"{roundedTimer}" : "GO";
-                var actualColor = roundedTimer >= 1f ? _timerStartColor : _timerEndColor;
+                var roundedSeconds = Mathf.RoundToInt(countdown);
+                var actualText = roundedSeconds >= 1f ? $"{roundedSeconds}" : "GO";
+                var actualColor = roundedSeconds >= 1f ? _timerStartColor : _timerEndColor;
 
-                _timer.text = actualText;
-                _timer.color = actualColor;
+                _countdown.text = actualText;
+                _countdown.color = actualColor;
 
-                yield return timer -= Time.deltaTime;
+                yield return countdown -= Time.deltaTime;
             }
 
-            _timerRoutine = null;
+            _countdownRoutine = null;
+        }
+
+        private IEnumerator Timer()
+        {
+            var timeElapsed = 0f;
+
+            while (true)
+            {
+                timeElapsed += Time.deltaTime;
+
+                int minutes = Mathf.FloorToInt(timeElapsed / 60);
+                int seconds = Mathf.FloorToInt(timeElapsed % 60);
+
+                string timeFormatted = string.Format("{0:00}:{1:00}", minutes, seconds);
+                _timer.text = timeFormatted;
+
+                yield return _waitForEndOfFrame;
+            }
         }
 
         private void InitCoinsBar()
         {
             _progressBar.maxValue = _matchManager.CoinsToWin;
             _progressBar.value = 0f;
+
+            _timer.gameObject.SetActive(false);
         }
 
-        private void RefreshCoinsCounter() => _counter.text = $"<color=yellow>{_matchManager.Coins}</color>/{_matchManager.CoinsToWin}";
+        private void RefreshCoinsCounter() => _counter.text = $"{_matchManager.Coins}/{_matchManager.CoinsToWin}";
 
         private void OnDisable()
         {
             Unsubscribe();
 
-            this.StopCoroutine(ref _timerRoutine);
+            StopTimer();
+            this.StopCoroutine(ref _countdownRoutine);
         }
     }
 }
